6c65e6ef6363fc3a6f9d95c168a73c066f62bf02
sari7happy <sari7happy@gmail.com>
2022-09-23 23:28:48 +0900

    ”auto_increment機能練習”

--- main.sql
+++ main.sql
@@ -1,6 +1,6 @@
+CREATE table test_db.test_table(
+	key1 INT AUTO_INCREMENT primary KEY
+    -- auto_incrementは自動でインデックスを割り当ててくれる機能。key1=primary key
+    
 
-create table test_db.test_table(
-    key1 int ,
-    key2 int,
-    PRIMARY key (key1,key2)
 );
