d074be3db1132c40303b50862630d4daaccccad1
sari7happy <sari7happy@gmail.com>
2022-09-23 22:31:52 +0900

    ”テーブルの確認方法”

--- main.sql
+++ main.sql
@@ -1,6 +1,1 @@
-use test_db;
--- use test_dbはtest_dbを省略できる。
-    create table test_table(
-        id int(6) unsigned default 0 comment 'ID',
-        val varchar(20) default 'hello' comment '値'
-    );
+show full columns from test_table;
