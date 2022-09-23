f11014e5babe35e1c870b3b95a7c47be6514e3ec
sari7happy <sari7happy@gmail.com>
2022-09-23 22:23:22 +0900

    ”useの使用方法”

--- main.sql
+++ main.sql
@@ -1,1 +1,6 @@
-drop table test_db.test_table;
+use test_db;
+-- use test_dbはtest_dbを省略できる。
+    create table test_table(
+        id int(6) unsigned default 0 comment 'ID',
+        val varchar(20) default 'hello' comment '値'
+    );
